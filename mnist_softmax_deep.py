f0632786dc84ea463307f1190b29943bff63435e
Hoo Chang Shin <hc7shin@gmail.com>
2018-06-03 22:26:23 -0400

    added deeper versions.

---
+++ mnist_softmax_deep.py
@@ -0,0 +1,78 @@
+"""A very simple MNIST classifier.
+See extensive documentation at
+https://www.tensorflow.org/get_started/mnist/beginners
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+import tensorflow as tf
+
+FLAGS = None
+
+
+def main(_):
+  # Import data
+  mnist = input_data.read_data_sets(FLAGS.data_dir)
+
+  # Create the model
+  x = tf.placeholder(tf.float32, [None, 784])
+  W1 = tf.Variable(tf.truncated_normal([784, FLAGS.num_hunits]))
+  b1 = tf.Variable(tf.zeros([FLAGS.num_hunits]))
+  z = tf.nn.relu(tf.matmul(x, W1) + b1)
+  W2 = tf.Variable(tf.truncated_normal([FLAGS.num_hunits, 10]))
+  b2 = tf.Variable(tf.zeros([10]))
+  y = tf.matmul(z, W2) + b2
+
+  # Define loss and optimizer
+  y_ = tf.placeholder(tf.int64, [None])
+
+  # The raw formulation of cross-entropy,
+  #
+  #   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
+  #                                 reduction_indices=[1]))
+  #
+  # can be numerically unstable.
+  #
+  # So here we use tf.losses.sparse_softmax_cross_entropy on the raw
+  # outputs of 'y', and then average across the batch.
+  cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=y)
+  train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+  sess = tf.InteractiveSession()
+  tf.global_variables_initializer().run()
+  # Train
+  for _ in range(3000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+  # Test trained model
+  correct_prediction = tf.equal(tf.argmax(y, 1), y_)
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  print(sess.run(
+      accuracy, feed_dict={
+          x: mnist.test.images,
+          y_: mnist.test.labels
+      }))
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--data_dir',
+      type=str,
+      default='/tmp/tensorflow/mnist/input_data',
+      help='Directory for storing input data')
+  parser.add_argument(
+      '--num_hunits',
+      type=int,
+      default=10,
+      help='Number of units in the hidden layer'
+  )
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
